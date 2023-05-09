5d55c57d1779ba58c8c880fd84d04b01b5705619
Sebastian <sebastian5253@wp.pl>
2023-05-09 20:06:50 +0200

    Create faketelemetry application

---
+++ src/main/java/com/example/faketelemetry/FaketelemetryApplication.java
@@ -0,0 +1,13 @@
+package com.example.faketelemetry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaketelemetryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaketelemetryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/faketelemetry/FaketelemetryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.faketelemetry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaketelemetryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
